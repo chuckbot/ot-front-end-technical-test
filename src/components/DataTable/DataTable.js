62b2893c9a40a05051dc36cec3b7d3380f88710e
Carlos Garcia <cgmilan@gmail.com>
2021-01-13 01:09:58 -0400

    Sort by highest overall association scores and expandable row

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,41 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./DataTable.css";
-
 import { DataProps } from "../../helpers/types";
 
 function DataTable(props) {
-    const { data } = props;
+  const { data } = props;
+  const [rows, setRows] = useState(<></>);
+  const [expand, setExpand] = useState([]);
 
-    const headers = (
-        <tr>
-            <th> </th>
-            <th>Symbol</th>
-            <th>Gene ID</th>
-            <th>Gene Name</th>
-            <th>Overall Association Score</th>
+  const headers = (
+    <tr>
+      <th> </th>
+      <th>Symbol</th>
+      <th>Gene ID</th>
+      <th>Gene Name</th>
+      <th>Overall Association Score</th>
+    </tr>
+  );
+
+  const handleExpand = (rowId) => {
+    const isExpanded = expand.includes(rowId);
+
+    const newExpanded = isExpanded
+      ? expand.filter((id) => id !== rowId)
+      : expand.concat(rowId);
+    setExpand(newExpanded);
+  };
+
+  const renderRow = (item) => {
+    const isExpanded = expand.includes(item.id);
+    const expandedLabel = isExpanded ? "-" : "+";
+
+    const itemRow = [
+      <tr key={item.id}>
+        <td className="expand" onClick={() => handleExpand(item.id)}>
+          {expandedLabel}
+        </td>
+        <td>{item.target.gene_info.symbol}</td>
+        <td>{item.target.id}</td>
+        <td>{item.target.gene_info.name}</td>
+        <td>{item.association_score.overall}</td>
+      </tr>,
+    ];
+
+    if (isExpanded) {
+      itemRow.push(
+        <tr key={`${item.id}-expanded`}>
+          <td colSpan="5">
+            <div>This is a test</div>
+          </td>
         </tr>
-    );
+      );
+    }
 
-    const rows = data.map((item) => (
-        <tr key={item.id}>
-            <td className="expand">+</td>
-            <td>{item.target.gene_info.symbol}</td>
-            <td>{item.target.id}</td>
-            <td>{item.target.gene_info.name}</td>
-            <td>{item.association_score.overall}</td>
-        </tr>
-    ));
+    return itemRow;
+  };
 
-    return (
-        <table>
-            <thead>{headers}</thead>
-            <tbody>{rows}</tbody>
-        </table>
+  useEffect(() => {
+    const sortedData = [...data];
+    sortedData.sort(
+      (a, b) => b.association_score.overall - a.association_score.overall
     );
+    setRows(sortedData.map((item) => renderRow(item)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, expand]);
+
+  return (
+    <table>
+      <thead>{headers}</thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
 
 DataTable.propTypes = {
-    data: DataProps
-}
+  data: DataProps,
+};
 
 export default DataTable;
